46e063fad1e324b69d55ed8d45180351db14531a
Shawn <myking123321@gmail.com>
2024-05-31 23:21:03 +0700

    Initial commit

---
+++ target/classes/module-info.java
@@ -0,0 +1,7 @@
+module com.mycompany.chatroom {
+    requires javafx.controls;
+    requires java.desktop;
+    requires java.logging;
+    exports com.mycompany.chatroom;
+    requires miglayout.swing;
+}
