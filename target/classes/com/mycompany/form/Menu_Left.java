805a2bca86a9e0c1d94a0a6d68b1f0aee11bab84
Shawn <myking123321@gmail.com>
2024-05-31 23:24:24 +0700

    iconMenu

--- target/classes/com/mycompany/form/Menu_Left.java
+++ target/classes/com/mycompany/form/Menu_Left.java
@@ -27,25 +27,76 @@ public class Menu_Left extends javax.swing.JPanel {
     private void initComponents() {
 
         jLayeredPane1 = new javax.swing.JLayeredPane();
+        menuButton1 = new com.mycompany.component.MenuButton();
+        menuButton2 = new com.mycompany.component.MenuButton();
+        menuButton3 = new com.mycompany.component.MenuButton();
 
         jLayeredPane1.setLayout(new javax.swing.BoxLayout(jLayeredPane1, javax.swing.BoxLayout.LINE_AXIS));
+
+        menuButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/icon/icons8-people-45-colored.png"))); // NOI18N
+        menuButton1.setMaximumSize(new java.awt.Dimension(43, 43));
+        menuButton1.setMinimumSize(new java.awt.Dimension(43, 43));
+        menuButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuButton1ActionPerformed(evt);
+            }
+        });
+        jLayeredPane1.add(menuButton1);
+
+        menuButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/icon/icons8-room-45-colored.png"))); // NOI18N
+        menuButton2.setMaximumSize(new java.awt.Dimension(43, 43));
+        menuButton2.setMinimumSize(new java.awt.Dimension(43, 43));
+        menuButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuButton2ActionPerformed(evt);
+            }
+        });
+        jLayeredPane1.add(menuButton2);
+
+        menuButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/icon/icons8-people-45-colored.png"))); // NOI18N
+        menuButton3.setMaximumSize(new java.awt.Dimension(43, 43));
+        menuButton3.setMinimumSize(new java.awt.Dimension(43, 43));
+        menuButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuButton3ActionPerformed(evt);
+            }
+        });
+        jLayeredPane1.add(menuButton3);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLayeredPane1)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLayeredPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(0, 0, 0)
-                .addComponent(jLayeredPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(497, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(jLayeredPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(487, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void menuButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_menuButton1ActionPerformed
+
+    private void menuButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_menuButton2ActionPerformed
+
+    private void menuButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_menuButton3ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLayeredPane jLayeredPane1;
+    private com.mycompany.component.MenuButton menuButton1;
+    private com.mycompany.component.MenuButton menuButton2;
+    private com.mycompany.component.MenuButton menuButton3;
     // End of variables declaration//GEN-END:variables
 }
